21319c297ccc477dacfd37523c3803216a366b3c
JoonasBjork <joonas.bjork@gmail.com>
2024-11-17 20:40:06 +0200

    Change id to a real styock symbol

--- scripts/push_to_redis_good.sh
+++ scripts/push_to_redis_good.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec redis redis-cli XADD s1 \* id id1 sectype I last 11.105000 time 09:19:44:743 date 08-11-2021
+docker exec redis redis-cli XADD s1 \* id ALCLA.FR sectype I last 11.105000 time 09:19:44:743 date 08-11-2021
