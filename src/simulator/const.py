3bd040a7ee8582f033eb2fc3c829d54400d76909
khaluan <50902621+khaluan@users.noreply.github.com>
2024-12-06 11:54:54 +0200

    Bring CSV_FILE and FILE_OFFSET_BYTES from simulator constant to env variable

--- src/simulator/const.py
+++ src/simulator/const.py
@@ -28,8 +28,8 @@ def get_int_envvar(envvar_name: str, default_value: Optional[int]) -> int:
     except Exception as e:
         raise RuntimeError(f"Unexpected error in get_int_envvar: {e}")
 
-CSV_FILE = '/data/debs2022-gc-trading-day-08-11-21.csv'
-FILE_OFFSET_BYTES = 3100000000
+CSV_FILE = get_str_envvar('CSV_FILE')
+FILE_OFFSET_BYTES = get_int_envvar("FILE_OFFSET_BYTES", 1)
 # When reading different files, use different offets
 # CSV_FILE = '/data/1_million_at_1_million_messages_per_5min.csv'
 # FILE_OFFSET_BYTES = 1 
