7f420886fd99c7d4b8e83d6b7ea0fc1d1fd9da97
khaluan <50902621+khaluan@users.noreply.github.com>
2024-12-06 15:06:13 +0200

    Remove the unused commented code in the simulator

--- src/simulator/csv_parser.py
+++ src/simulator/csv_parser.py
@@ -53,25 +53,28 @@ def read_batch_from_offset(filename: str, start_offset: int, chunk_size: int = 1
             next_line = file.readline()
             if len(list(csv.reader([next_line]))[0]) == 40:
                 break
-    
-    chunks = pd.read_csv(
-        file,
-        skip_blank_lines=False,
-        chunksize=chunk_size,
-        index_col=False        
-    )
+    try:
+        chunks = pd.read_csv(
+            file,
+            skip_blank_lines=False,
+            chunksize=chunk_size,
+            index_col=False        
+        )
 
-    for chunk in chunks:
-        for _, line in chunk.iterrows():
-            yield line.tolist()
+        for chunk in chunks:
+            for _, line in chunk.iterrows():
+                yield line.tolist()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"""
+            Error: File not found. Looking for file in folder /data/. Instead the file is at {filename}
+            Please check your CSV_FILE in the .env file. Make sure it is in the format /data/csv_filename.csv
+        """) from e
 
 def get_next_n(iterator, n):
     return list(islice(iterator, n))
 
 def parser_run(csv_file: str, queue) -> None:
     try:
-
-        # Start the file from 15:00
         generator = read_batch_from_offset(csv_file, FILE_OFFSET_BYTES)
         first_record = next(generator)
         while pd.isna(first_record[TIME_OFFSET]):
@@ -79,9 +82,8 @@ def parser_run(csv_file: str, queue) -> None:
             
         time_shift = datetime.now() - parse_time(first_record[TIME_OFFSET])
      
-
-        print("[PARSER] Starting to add items to queue")
-        print("[PARSER] first_record", first_record)
+        # print("[PARSER] Starting to add items to queue")
+        # print("[PARSER] first_record", first_record)
 
         try:
             iter = 0
@@ -110,56 +112,12 @@ def parser_run(csv_file: str, queue) -> None:
                 if iter % 1000 == 0:
                     print("[PARSER] iter:", iter)
                     print("[PARSER] Parser lag:", -sleep_duration, "s")
-                    # print("Record:", record)
                 
 
                 queue.send(csv_row_to_redis(record, current_time))
-                # queue.put(csv_rsow_to_redis(record, timestamp))
                 
         except StopIteration:
             return
     except KeyboardInterrupt:
         return
     
-
-# def parse_csv(csv_file: str) -> Generator[list[str], None, None]:
-#     """ Create a generator that read the csv line by line """
-#     with open(csv_file, mode='r') as file:
-#         reader = csv.reader(file)
-#         for _ in range(13):
-#             header = next(reader)
-#         for row in reader:
-#             yield row
-
-# def read_from_offset(filename: str, start_offset: int, buffer_size: int = 128) -> Generator[str, None, None]:
-#     """Generator to read from a specific offset in the file and yield lines."""
-#     with open(filename, 'r') as file:
-#         # Seek to the starting offset
-#         file.seek(start_offset)
-
-#         # Temporary buffer to hold chunks of file data
-#         buffer = ''
-
-#         reader = csv.reader(iter(file))
-        
-#         while True:
-#             # Read a chunk from the file
-#             chunk = file.read(buffer_size)
-            
-#             if not chunk:
-#                 break  # End of file reached
-            
-#             # Add the chunk to the buffer
-#             buffer += chunk
-
-#             # Process the buffer, line by line
-#             while '\n' in buffer:
-#                 # line, buffer = buffer.split('\n', 1)  # Split at the first newline
-
-#                 row = next(reader)
-#                 yield row  # Yield the line, stripped of any extra whitespace
-
-#         # Yield any remaining data in the buffer that wasn't followed by a newline
-#         if buffer:
-#             row = next(reader)
-#             yield row
--- src/simulator/main.py
+++ src/simulator/main.py
@@ -8,17 +8,10 @@ from publisher import publisher_run_redis # publisher_run_http,
 from const import CSV_FILE, PUBLISHER_COUNT
 
 
-# parser_thread = threading.Thread(target=parser_run, args=[CSV_FILE, task_queue])
-# publisher_thread = threading.Thread(target=publisher_run_redis, args=[task_queue])
-
 receive_conn, produce_conn = Pipe()
 parser_thread = Process(target=parser_run, args=[CSV_FILE, produce_conn])
 publisher_thread = Process(target=publisher_run_redis, args=[receive_conn])
 
-# task_queue = Queue()
-# parser_thread = Process(target=parser_run, args=[CSV_FILE, task_queue])
-# publisher_threads = [Process(target=publisher_run_redis, args=[task_queue]) for _ in range(PUBLISHER_COUNT)]
-# print(f"{PUBLISHER_COUNT} publisher created")
 # Start the threads
 parser_thread.start()
 # for publisher_thread in publisher_threads:
